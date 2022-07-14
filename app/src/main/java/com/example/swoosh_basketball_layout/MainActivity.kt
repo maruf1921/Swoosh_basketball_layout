4444a825dff2d000011b53f2673a383d9524b0ff
maruf <maruf1921@gmail.com>
2022-07-14 13:46:43 +0600

    connect first page to secound page.

--- app/src/main/java/com/example/swoosh_basketball_layout/MainActivity.kt
+++ app/src/main/java/com/example/swoosh_basketball_layout/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.swoosh_basketball_layout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, activity_league::class.java)
+            startActivity(leagueIntent)
+        }
+
     }
 }
