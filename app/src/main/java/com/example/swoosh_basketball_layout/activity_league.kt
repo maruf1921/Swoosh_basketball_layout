91d99e2d3f932d6022927f5af4665ef36970f3bb
maruf <maruf1921@gmail.com>
2022-07-14 12:21:24 +0600

    activity league (2nd page done)

---
+++ app/src/main/java/com/example/swoosh_basketball_layout/activity_league.kt
@@ -0,0 +1,11 @@
+package com.example.swoosh_basketball_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_league : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
