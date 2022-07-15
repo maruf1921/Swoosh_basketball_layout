2c4a17d31add6ee08917d4ffbc4ab47d6e085f46
maruf <maruf1921@gmail.com>
2022-07-15 21:08:50 +0600

    secound page to third page & have to select something in 2nd page

---
+++ app/src/main/java/com/example/swoosh_basketball_layout/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.swoosh_basketball_layout
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/swoosh_basketball_layout/activity_league.kt
+++ app/src/main/java/com/example/swoosh_basketball_layout/activity_league.kt
@@ -1,11 +1,49 @@
 package com.example.swoosh_basketball_layout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class activity_league : AppCompatActivity() {
+
+    var selectedLeague = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun onMensClicked(view: View){
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View){
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View){
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = "co-ed"
+    }
+
+    fun leagueNextClicked(view: View){
+        if (selectedLeague != ""){
+        val skillActivity = Intent(this,skill_activity::class.java)
+        skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+    } else{
+            Toast.makeText(this, "please, select a league", Toast.LENGTH_SHORT).show()
+        }
     }
 }
